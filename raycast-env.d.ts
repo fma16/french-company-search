64287b7010132b9b0e806d38aa67063b145db906
Paul-Henri Laugier <github@laugier.co>
2025-10-29 21:47:57 +0100

    Regenerate Raycast preference types

--- raycast-env.d.ts
+++ raycast-env.d.ts
@@ -15,7 +15,7 @@ type ExtensionPreferences = {
   /** Read Clipboard on Launch - Automatically paste SIREN/SIRET or company name from the clipboard when the extension opens. */
   "autoReadClipboard": boolean,
   /** Output Template - Define your custom output template. Use {{variable}} for placeholders. See documentation for available variables. */
-  "outputTemplate"?: unknown
+  "outputTemplate"?: string
 }
 
 /** Preferences accessible in all the extension's commands */
